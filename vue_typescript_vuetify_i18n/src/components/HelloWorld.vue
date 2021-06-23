b49d6d91d7a881683544b8e2b181ac0c32c57e99
Spy1ne <mazal.mathieu@gmail.com>
2021-06-23 16:48:02 +0200

    Cardsprojects + img source

--- vue_typescript_vuetify_i18n/src/components/HelloWorld.vue
+++ vue_typescript_vuetify_i18n/src/components/HelloWorld.vue
@@ -1,18 +1,29 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+      <v-col md="3" cols="12">
+        <pf-Project/>
+        <pf-CardsProjects src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"/>
+      </v-col>
+      <v-col md="3" cols="12">
+        <pf-Project/>
+        <pf-CardsProjects src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"/>
+      </v-col>
+      <v-col md="3" cols="12">
+        <pf-Project/>
+        <pf-CardsProjects src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"/>
+      </v-col>
+      <v-col md="3" cols="12">
+        <pf-Project/>
+        <pf-CardsProjects src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"/>
       </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           {{$t('message')}}
+        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">
+          {{$t('salut')}}
         </h1>
       </v-col>
     </v-row>
@@ -23,9 +34,6 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'HelloWorld',
-
-  data: () => ({
-  })
+  name: 'HelloWorld'
 })
 </script>
--- vue_typescript_vuetify_i18n/src/plugins/components.ts
+++ vue_typescript_vuetify_i18n/src/plugins/components.ts
@@ -4,7 +4,11 @@ import Vue from 'vue'
 import HeaderComponent from '@/components/header/TheHeader.component.vue'
 import SelectLocale from '@/components/select-locale/TheSelectLocale.component.vue'
 import FooterComponent from '@/components/footers/basdepage.component.vue'
+import ProjectBottom from '@/components/ProjectBottomSheet/ProjectBottom.component.vue'
+import CardsProjects from '@/components/cards-projects/CardsProjects.component.vue'
 
 Vue.component('pf-header', HeaderComponent)
 Vue.component('pf-locale', SelectLocale)
 Vue.component('pf-Footer', FooterComponent)
+Vue.component('pf-Project', ProjectBottom)
+Vue.component('pf-CardsProjects', CardsProjects)
