6bbbfb80d421eff73eb3068523376092a100e74e
Spy1ne <mazal.mathieu@gmail.com>
2021-06-25 20:56:27 +0200

    presentation home adaptive

--- vue_typescript_vuetify_i18n/src/components/presentation-home-page/PresentationHomePage.component.vue
+++ vue_typescript_vuetify_i18n/src/components/presentation-home-page/PresentationHomePage.component.vue
@@ -1,15 +1,36 @@
 <template>
     <v-container>
-        <h1 class="font-weight-medium">MAZAL Mathieu</h1>
-        <h3>Etudiant troisième années Bachelor ESDAC</h3>
-        <p>
-
-        </p>
+        <v-card flat color="transparent" align="center" justify="center">
+            <v-row align="center" justify="center">
+                <v-col cols="12" :sm="smti" :lg="lgti">
+                    <v-avatar :width="sizei" :height="sizei">
+                        <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+                    </v-avatar>
+                </v-col>
+                <v-col align="left" class="ml-5" cols="12" :sm="smt" :lg="lgt">
+                    <h1 class="font-weight-medium">MAZAL Mathieu</h1>
+                    <h3>Etudiant troisième années Bachelor ESDAC</h3>
+                    <p><br>
+                    Cette année j'ai eu l'opportunité de travailler sur différents types de projet comme la réhabilitation<br>
+                    de musée symbolisant les œuvres présentes, une habitation dédiée aux personnes âgées,<br>
+                    une habitation dans un espace réduit et son extérieur, un aménagement d'un espace<br>
+                    symbolisant une marque et d’autres projets toujours plus variés.
+                    </p>
+                </v-col>
+            </v-row>
+        </v-card>
     </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
+  data: () => ({
+    smt: '8',
+    lgt: '7',
+    smti: '4',
+    lgti: '2',
+    sizei: '100%'
+  })
 })
 </script>
