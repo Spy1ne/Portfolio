83567f4710adea9c2f67215e94e72fb6f89bde2e
Spy1ne <mazal.mathieu@gmail.com>
2021-06-26 16:16:21 +0200

    presentation sheet parcour

--- vue_typescript_vuetify_i18n/src/components/presentation-home-page/PresentationHomePage.component.vue
+++ vue_typescript_vuetify_i18n/src/components/presentation-home-page/PresentationHomePage.component.vue
@@ -8,14 +8,53 @@
                     </v-avatar>
                 </v-col>
                 <v-col align="left" class="ml-5" cols="12" :sm="smt" :lg="lgt">
-                    <h1 class="font-weight-medium">MAZAL Mathieu</h1>
-                    <h3>Etudiant troisième années Bachelor ESDAC</h3>
-                    <p><br>
-                    Cette année j'ai eu l'opportunité de travailler sur différents types de projet comme la réhabilitation<br>
-                    de musée symbolisant les œuvres présentes, une habitation dédiée aux personnes âgées,<br>
-                    une habitation dans un espace réduit et son extérieur, un aménagement d'un espace<br>
-                    symbolisant une marque et d’autres projets toujours plus variés.
+                    <h1 class="font-weight-medium">{{$t('PresentationHomePage.name')}}</h1>
+                    <h3>{{$t('PresentationHomePage.school')}}</h3>
+                    <p>
+                        <br>{{$t('PresentationHomePage.Description.paragraph1')}}
+                        <br><br>{{$t('PresentationHomePage.Description.paragraph2')}}
+                        <br><br>{{$t('PresentationHomePage.Description.paragraph3')}}
                     </p>
+                    <v-col align="center" class="ml-5">
+                        <v-btn  @click="sheet = true" elevation="5" rounded>{{$t('PresentationHomePage.button')}}
+                            <v-bottom-sheet v-model="sheet">
+
+                                <v-sheet class="text-center" height="1000px">
+                                <v-card v-scroll.self="onScroll" class="overflow-y-auto" max-height="1000px">
+                                    <v-btn class="mt-6" text color="red" @click="sheet = !sheet">quitter</v-btn>
+                                    <h1 class="font-weight-medium">{{$t('PresentationHomePage.name')}}</h1>
+                                    <h3>{{$t('PresentationHomePage.school')}}</h3>
+
+                                    <v-card align="center" justify="center" flat color="transparent">
+                                    <v-col cols="12" :sm="smt" :lg="lgt">
+                                        <v-timeline float>
+                                            <v-timeline-item color="red lighten-2" small fill-dot>
+                                            <span slot="opposite">2019-2022</span>
+                                            <v-card dark color="red lighten-2">
+                                                <h3>ESDAC (Clermont-Ferrand)</h3>
+                                            <v-card-text class="white text--primary">
+                                                <p>3éme année de Bachelor Design d'Espace</p>
+                                            </v-card-text>
+                                            </v-card>
+                                            </v-timeline-item>
+                                            <v-timeline-item color="red lighten-2" small fill-dot>
+                                            <span slot="opposite">Mars 2020</span>
+                                            <v-card dark color="red lighten-2">
+                                                <h3>Stage cabinet d'architecte (Thuret)</h3>
+                                            <v-card-text class="white text--primary">
+                                                <p>Atelier Richard DUMAS (Thuret)<br>Etude et conception d'aménagement intérieur.<br>Dessin et réalisation de mobiliers.</p>
+                                            </v-card-text>
+                                            </v-card>
+                                            </v-timeline-item>
+                                        </v-timeline>
+                                    </v-col>
+                                    </v-card>
+                                    </v-card>
+                                </v-sheet>
+
+                            </v-bottom-sheet>
+                        </v-btn>
+                    </v-col>
                 </v-col>
             </v-row>
         </v-card>
@@ -27,10 +66,17 @@ import Vue from 'vue'
 export default Vue.extend({
   data: () => ({
     smt: '8',
-    lgt: '7',
+    lgt: '6',
     smti: '4',
     lgti: '2',
-    sizei: '100%'
-  })
+    sizei: '100%',
+    sheet: true,
+    show: false
+  }),
+  methods: {
+    onclick: function () {
+      console.log('click')
+    }
+  }
 })
 </script>
