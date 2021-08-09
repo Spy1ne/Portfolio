9919275d851767761a7129152a692b9d42c8fe81
Spy1ne <mazal.mathieu@gmail.com>
2021-08-09 22:04:24 +0200

    update color cv

--- vue_typescript_vuetify_i18n/src/components/presentation-home-page/PresentationHomePage.component.vue
+++ vue_typescript_vuetify_i18n/src/components/presentation-home-page/PresentationHomePage.component.vue
@@ -2,18 +2,12 @@
     <div>
         <v-card flat color="transparent" align="center" justify="center" class="mt-5 mb-5">
             <v-row align="center" justify="center">
-                <v-col cols="12" :sm="smti" :lg="lgti">
-                    <v-avatar :width="sizei" :height="sizei">
-                        <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
-                    </v-avatar>
-                </v-col>
-                <v-col align="left" cols="12" :sm="smt" :lg="lgt">
+                <v-col class="mt-4 mb-3" cols="12" lg="8">
                     <h1 class="font-weight-medium">{{ $t("PresentationHomePage.name") }}</h1>
                     <h3>{{ $t("PresentationHomePage.school") }}</h3>
-                    <p>
+                    <p align="left">
                     <br />{{ $t("PresentationHomePage.Description.paragraph1") }}
                     <br /><br />{{ $t("PresentationHomePage.Description.paragraph2") }}
-                    <br /><br />{{ $t("PresentationHomePage.Description.paragraph3") }}
                     </p>
                 </v-col>
             </v-row>
@@ -26,18 +20,23 @@
         <v-bottom-sheet v-model="sheet" >
             <v-sheet class="text-center" min-height="300px">
                 <div align="center" class="sheet_container">
+                    <v-divider color="#008080"/>
+                    <v-divider color="#008080"/>
+                    <v-divider color="#008080"/>
                     <v-container fluid class="sheet_item">
-                         <v-card min-width="400px" max-width="50vw" flat color="transparent">
-                        <v-btn class="mt-5 mb-2" text color="blue-grey darken-1" @click="sheet = !sheet">
+                    <v-card min-width="400px" max-width="50vw" flat color="transparent">
+                        <v-btn class="mt-5 mb-6" outlined color="white" @click="sheet = !sheet">
                             quitter
                         </v-btn>
                         <h1 class="font-weight-medium">{{ $t("PresentationHomePage.name") }}</h1>
-                        <h3 class="mb-5">{{ $t("PresentationHomePage.school") }}</h3>
-                        <v-card dark color="teal darken-2">
+                        <h3 class="mb-7">{{ $t("PresentationHomePage.school") }}</h3>
+                        <v-card outlined color="">
+                            <v-card class="mt-0" color="teal darken-2 !important">
                             <h3>Competences</h3>
+                            </v-card>
                             <v-card-text min-height="70" align="center" class="white text--primary">
                                 <v-row justify="center">
-                                    <v-col cols="2" md="auto">
+                                    <v-col color="" cols="2" md="auto">
                                         <v-tooltip bottom>
                                             <template v-slot:activator="{on, attrs}">
                                                 <v-img width="57px" src="@/assets/logo-soft/rhino_50px_50px.png" v-on="on" v-bind="attrs"/>
@@ -113,19 +112,34 @@
                             </v-card-text>
                         </v-card>
                         <v-timeline class="text-center">
-                            <v-timeline-item icon="mdi-school" color="teal darken-1" fill-dot>
+                            <v-timeline-item class="mt-3" icon="mdi-school" color="teal darken-1" fill-dot>
                                 <span class="font-weight-bold" slot="opposite">2019-2022</span>
-                                <v-card dark color="teal darken-1">
+                                <v-card outlined >
+                                    <v-card color="teal darken-2 !important" >
                                     <h3>ESDAC (Clermont-Ferrand)</h3>
+                                    </v-card>
                                     <v-card-text class="white text--primary">
                                         <p>3éme année de Bachelor Design d'Espace</p>
                                     </v-card-text>
                                 </v-card>
                             </v-timeline-item>
+                             <v-timeline-item icon="mdi-account-tie" color="teal lighten-1" fill-dot>
+                                <span class="font-weight-bold" slot="opposite">Juillet-Août 2021</span>
+                                <v-card outlined>
+                                    <v-card color="teal darken-2 !important" >
+                                    <h3>Stage cabinet d'architecte (Vichy)</h3>
+                                    </v-card>
+                                    <v-card-text class="white text--primary">
+                                        <p>Agence DOUX Architecte<br />Participation aux projets<br />Dessin et réalisation de plan 2D et rendu 3D</p>
+                                    </v-card-text>
+                                </v-card>
+                            </v-timeline-item>
                             <v-timeline-item icon="mdi-account-tie" color="teal lighten-1" fill-dot>
                                 <span class="font-weight-bold" slot="opposite">Mars 2020</span>
-                                <v-card dark color="teal lighten-1">
+                                <v-card outlined>
+                                    <v-card color="teal darken-2 !important" >
                                     <h3>Stage cabinet d'architecte (Thuret)</h3>
+                                    </v-card>
                                     <v-card-text class="white text--primary">
                                         <p>Atelier Richard DUMAS (Thuret)<br />Etude et conception
                                         d'aménagement intérieur.<br />Dessin et réalisation de
@@ -135,8 +149,10 @@
                             </v-timeline-item>
                             <v-timeline-item icon="mdi-briefcase-variant" color="teal lighten-2" fill-dot>
                                 <span class="font-weight-bold" slot="opposite">2016-2018</span>
-                                <v-card dark color="teal lighten-2">
+                                <v-card outlined>
+                                    <v-card color="teal darken-2 !important" >
                                     <h3>Professionnelles</h3>
+                                    </v-card>
                                     <v-card-text class="white text--primary">
                                         <p>Contrat 9 mois Laboratoire GEN-BIO (Clermont-ferrand)
                                         <br>Contrat 2 ans EHPAD (Aigueperse)</p>
@@ -156,10 +172,7 @@
 import Vue from 'vue'
 export default Vue.extend({
   data: () => ({
-    smt: '8',
     lgt: '6',
-    smti: '4',
-    lgti: '2',
     sizei: '100%',
     sheet: false,
     show: false,
@@ -187,5 +200,11 @@ border-right: 1px #f8f7f3 solid;
 height:90vh;
 overflow-y:auto;
 position:relative;
+}
+.v-application .white {
+    background-color: #e2e2e200 !important
+}
+.theme--dark.v-sheet--outlined {
+    border: thin solid #008080 !important
 }
 </style>
