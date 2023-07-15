cc55ee78bb729603e163759fa1748f27d08433f1
Mathieu M <86263256+mathieumzl@users.noreply.github.com>
2023-07-15 20:53:47 +0200

    Update HomeCover.component.vue

--- vue_typescript_vuetify_i18n/src/components/home-cover/HomeCover.component.vue
+++ vue_typescript_vuetify_i18n/src/components/home-cover/HomeCover.component.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container fluid pa-0>
-    <video width="100%" disablePictureInPicture preload="auto" muted playsinline autoplay loop id="headerVideo">
+    <!--<video width="100%" disablePictureInPicture preload="auto" muted playsinline autoplay loop id="headerVideo">
       <source src='@/assets/carousel/carousel.mp4' type='video/mp4'/>
     </video>
     <v-divider class="mt-n1" opacity="0" color='rgba(109,91,58,0)'/>
-    <v-divider class="mt-n1" color="white"/>
+    <v-divider class="mt-n1" color="white"/>-->
         <!-- <v-card class='pa-0 rounded-0' elevation='10'>
             <v-carousel cycle height="auto" fluid pa-0 show-arrows-on-hover hide-delimiters>
           <v-carousel-item v-for="(item,i) in items" :key="i">
